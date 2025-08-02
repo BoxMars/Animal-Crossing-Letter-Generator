284a6b033bc797395af8d232e65a0ae925f4c158
Idrees Hassan <idrees@idreesinc.com>
2025-08-01 20:10:03 -0400

    Add all default stationary

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -2,30 +2,50 @@ import "./Card.css";
 
 export enum CardName {
   Airmail = "Airmail",
-  BirthdayCake = "Birthday Cake",
-  BlueSky = "Blue Sky",
-  BunnyDay = "Bunny Day",
-  CherryBlossoms = "Cherry Blossoms",
-  Common = "Common",
-  Dandelion = "Dandelion",
-  ElegantRoses = "Elegant Roses",
-  FantasyStars = "Fantasy Stars",
-  FathersDay = "Father's Day",
-  FullBloom = "Full Bloom",
-  Gears = "Gears",
+  HappyClovers = "Happy-Clovers",
+  BlueSky = "Blue-Sky",
+  BirthdayCake = "Birthday-Cake",
+  Balloons = "Balloons",
+  Star = "Star",
+  Bandage = "Bandage",
+  FantasyStars = "Fantasy-Stars",
+  BabyGoods = "Baby-Goods",
+  Ribbon = "Ribbon",
+  FlowerBouquet = "Flower-Bouquet",
   Gem = "Gem",
+  CoolCool = "Cool-Cool",
+  LovelyHearts = "Lovely Hearts",
+  Shapes = "Shapes",
   Goldfish = "Goldfish",
   Graffiti = "Graffiti",
+  Fanciful = "Fanciful",
+  Common = "Common",
+  Decorative = "Decorative",
+  Graduation = "Graduation",
+  RedDragonflies = "Red-Dragonflies",
+  Camo = "Camo",
+  Zen = "Zen",
+  Wedding = "Wedding",
+  ElegantRoses = "Elegant-Roses",
+  Torn = "Torn",
+  SoManyHearts = "So-Many-Hearts",
+  ShootingStars = "Shooting-Stars",
+  StationaryGoods = "Stationary-Goods",
+  Velvety = "Velvety",
+  Gears = "Gears",
+  DawningYear = "Dawning-Year",
+  Fireworks = "Fireworks",
+  // Seasonal
+  BunnyDay = "Bunny-Day",
+  CherryBlossoms = "Cherry-Blossoms",
+  Dandelion = "Dandelion",
+  FathersDay = "Father's Day",
+  FullBloom = "Full Bloom",
   Hibiscus = "Hibiscus",
   MothersDay = "Mother's Day",
-  Fireworks = "Fireworks",
   Dragonflies = "Dragonflies",
-  ShootingStars = "Shooting Stars",
-  SoManyHearts = "So Many Hearts",
-  StationaryGoods = "Stationary Goods",
-  Clouds = "Clouds",
+  FluffyClouds = "Fluffy-Clouds",
   Beach = "Beach",
-  Velvety = "Velvety"
 }
 
 function getCssClass(type: CardName): string {
@@ -38,7 +58,7 @@ export default function Card({ type = CardName.Airmail }: { type?: CardName }) {
       <div className="card-start">
         Dear Fink,
       </div>
-      <div className="card-message">
+      <div className="card-message" contentEditable={true}>
         Congratulations on your big move! We hope you enjoy your new island life. To celebrate this fresh start, we'd like to give you a gift that is sure to come in handy!
       </div>
       <div className="card-signature">
